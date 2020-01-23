2e1a5ea7d194632681b976f8c2b35ca7653340ea
wolfzerax <cleiton.contato2210@gmail.com>
2020-01-23 13:42:49 -0300

    iniciando a tarefa 10

---
+++ Tarefa 10/FeiraoOnline/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { HomeComponent } from './home/home.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListagemComponent } from './listagem/listagem.component';
+import { EditarComponent } from './editar/editar.component';
+import { LoginComponent } from './login/login.component';
+import { DuvidasComponent } from './duvidas/duvidas.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'/home', pathMatch:'full'},
+  {path:'quemsomos', component: QuemSomosComponent},
+  {path:'contato', component:ContatoComponent},
+  {path:'produtos', component:ProdutosComponent},
+  {path:'home', component:HomeComponent},
+  {path:'cadastro', component:CadastroComponent},
+  {path:'listagem', component:ListagemComponent},
+  {path:'editar/:id', component: EditarComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'duvidas', component: DuvidasComponent}
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
