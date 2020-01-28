32c3752effdf0e5dce3390cfe65163867c0585ae
Maywhy <mayara.cdb@gmail.com>
2020-01-28 14:21:20 -0300

    Criação do Banco de Dados

---
+++ Tarefa 11/Banco de Dados/BDFeirao.sql
@@ -0,0 +1,56 @@
+create database feiraoonline;
+use feiraoonline;
+
+create table cliente (
+	idcliente	integer not null auto_increment,
+	nome		varchar (100),
+	endereco	varchar (100),
+	cidade 		varchar (50),
+	estado 		varchar (50),
+	cep			varchar (10),
+	email		varchar (100),
+	senha		varchar (50),
+	telefone	varchar (20),
+
+	constraint pk_cliente primary key (idcliente)
+);
+
+create table categoria (
+	idcategoria	integer not null auto_increment,
+	categoria	varchar (20),
+    
+    constraint pk_categoria primary key (idcategoria)
+);
+
+create table produto (
+	idproduto	integer not null auto_increment,
+	idcategoria	integer not null,
+	titulo		varchar (100),
+	descricao	varchar (200),
+	linkfoto	text,
+	preco		float,
+    
+    constraint pk_produto 	primary key (idproduto),
+    constraint fk_categoria	foreign key (idcategoria) references categoria (idcategoria)
+);
+
+create table pedido (
+	idpedido	integer not null auto_increment,
+	idproduto	integer not null ,
+	idcliente	integer not null,
+	quantidade	integer,
+	dtpedido	date,
+    
+    constraint pk_pedido 	primary key (idpedido),
+	constraint fk_prod 	foreign key (idproduto) references produto (idproduto), 
+    constraint fk_cliente 	foreign key (idcliente) references cliente (idcliente)
+);
+
+create table entrega (
+	identrega	integer not null auto_increment,
+	idpedido	integer not null,
+	andamento	boolean,
+    
+    constraint pk_entrega	primary key (identrega),
+    constraint fk_pedido	foreign key (idpedido) references pedido (idpedido)
+);
---
+++ Tarefa 11/Banco de Dados/InsertsFeirao.sql
@@ -0,0 +1,160 @@
+-- INSERT CLIENTE    
+insert into cliente values (null, "João Vitor Soares Lima", "Rua Edson Mota n37B", "São Paulo", "São Paulo", "08460-465",
+    "joaovitorsoareslima12@gmail.com", "1234567891@", "(11)9402-26032");
+    
+insert into cliente values (null, "Debora", "Rua Manuel Pinto n65", "São Paulo", "São Paulo", "04589-589", "debora@gmail.com",
+    "1234567891@", "(11)9875-56897");
+    
+insert into cliente values (null, "Cleiton", "Rua Isidro Mota n66A", "São Paulo", "São Paulo", "012359-436", "cleiton@gmail.com",
+    "1234567891@", "(11)9298-87529");
+    
+insert into cliente values (null, "Mayara", "Rua Thiago Truta n77", "São Paulo", "São Paulo", "78564-546", "mayara@gmail.com",
+    "1234567891@", "(11)9574-56982");
+    
+insert into cliente values (null, "Felipe", "Rua Marques n42", "São Paulo", "São Paulo", "45684-203", "mayara@gmail.com",
+    "1234567891@", "(11)9554-12345");
+    
+insert into cliente values (null, "Adrian", "Rua Mazola n43", "São Paulo", "São Paulo", "78952-456", "adrian@gmail.com",
+    "1234567891@", "(11)9402-26032");
+    
+insert into cliente values (null, "Laura", "Rua Nabis n420", "São Paulo", "São Paulo", "45628-169", "laura@gmail.com", 
+    "1234567891@", "(11)9158-66666");
+    
+insert into cliente values (null, "Thaigo", "Rua Fonseca n66", "São Paulo", "São Paulo", "00874-421", "thiago@gmail.com", 
+    "1234567891@", "(11)426-12358");
+    
+insert into cliente values (null, "Isidro", "Rua Oz n12B", "Osasco", "São Paulo", "02156-854", "isidro@gmail.com", 
+    "1234567891@", "(11)9999-99999");
+    
+insert into cliente values (null, "Aline", "Rua Aline n45", "São Paulo", "São Paulo", "00125-854", "aline@gmail.com",
+    "1234567891@", "(11)9123-45698");
+    
+insert into cliente values (null, "Leonardo", "Rua Copa n48", "São Paulo", "São Paulo",	"02548-895", "leo@gmail.com", 
+    "1234567891@", "(11)9874-85759");
+    
+insert into cliente values (null, "Guilherme", "Rua Edson Mota n37B", "São Paulo", "São Paulo", "08460-465", "gui@gmail.com", 
+    "1234567891@", "(11)9485-85697");
+    
+insert into cliente values (null, "Fernanda", "Rua Edson Mota n37B", "São Paulo","São Paulo", "08460-465", "fernanda@gmail.com",
+    "1234567891@", "(11)9402-99999");
+    
+insert into cliente values (null, "Rosa", "Rua Naria das dores n37", "São Paulo", "São Paulo", "05468-892", "rosa@gmail.com",
+    "1234567891@", "(11)9582-45612");
+    
+insert into cliente values (null, "Paulo", "Rua Augusta n21", "São Paulo", "São Paulo", "00025-854", "paulo@gmail.com", 
+     "1234567891@", "(11)9523-13245");
+    
+-- INSERT CATEGORIA    
+insert into categoria values (null, "Fruta");
+    
+insert into categoria values (null, "Legume");
+    
+insert into categoria values (null, "verdura");
+    
+-- INSERT PRODUTO    
+insert into produto values(null, 1,"Maça Argentina", "Maça argetina importada vermelha, doce e crocante",
+	"https://cd.shoppub.com.br/cenourao/media/cache/db/fc/dbfcc0256d4c1d2814a88be81511bb68.jpg", 5.00);
+    
+insert into produto values(null, 1, "Manga Palmer", "Manga Palmer Baiana, doce e grauda",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3pWv3M1aQqGSxohHGgJldks0jE8HXYgoZWcFg_VHd9b9iCboN4g&s", 5.00);
+    
+insert into produto values(null, 1, "Mamão Papaia", "Mamão Papaia vermelho e doce ",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc7FUvYQKgN6MPSHASlFJDV8Ho-xcYENXhy4Q4rZNCJ8Gys3dz3Q&s", 5.00);
+    
+insert into produto values(null, 1, "Maça Fuji", "Maça Fuji saborada e macia",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRms3YqaiYNWf2kLreg7eEqI0HM4jwOUCt70ctS2-TyrhQfybNQFw&s", 5.00);
+    
+insert into produto values(null, 1, "Uva Crinson sem semente", "Uva sem semente importada do chile",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW7sfmaa3lpKZwqY1Un8u-XnoXm9Z7D5hpitdzBV46S5DauVbj&s", 12.00);
+    
+insert into produto values(null, 1, "Uva Thompson sem semente", "Uva sem semente importada do chile",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs-cRyHDIehAVcVn51iadg_-7jIoSUJT89_OE6-rjUVD3a8n3Bdg&s", 12.00);
+    
+insert into produto values(null, 1, "Uva Italia", "Uva italia doce",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw7Mk0er_KKP5jBeLCKBaDWgGHehdTM7GUQ9XvuldOY4Ru7SCn&s", 10.00);
+    
+insert into produto values(null, 1, "Uva Rosada", "Uva Rosada boa demais",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROP-UMoZtEba-vLRsi7qpYsd1QUZcjxiMiPd4BfiupT9T0bQOa6A&s", 10.00);
+    
+insert into produto values(null, 2, "Pimentao Vermelho", "Pimentao vermelho de arder os olhos",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6xdi6jXRXFkiKV053T6p5zsjTeaTVQtyPsz04fndDyPSU_1az&s", 4.00);
+    
+insert into produto values(null, 2, "Pimentao Verde", "Pimentao verde de arder os olhos",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK94T-XLrevJ0TVPmgoGQYEi7Y0U-1ANspvga575-WE7Z-ScVA&s", 4.00);
+    
+insert into produto values(null, 2, "Pimentao Amarelo", "Pimentao amarelo de arder os olhos",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzrZ_HeUzr9GalJf1LEhn-zpgIlgeeZRLkNjKynYUbLUmubiJP&s", 4.00);
+    
+insert into produto values(null, 3, "Alface Crespa", "Alface crespa fresca e limpa",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3mEFh9LN2HNkEkLEQk_YJLN09uUfUwNT8x55SVOXdaNSLFogMBA&s", 4.00);
+    
+insert into produto values(null, 3, "Alface Lisa", "Alface Lisa fresca e limpa",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQ6GdaKpROsDM6oGPLWgmBhIj42XOt3y69bTFLp59ncJ_ksxyxhg&s", 4.00);
+    
+insert into produto values(null, 3, "Manjericão", "Manjericão",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyOBKNr6Ax4OYb1pHD-NMiYl8ef4ItZk1SThT2Q6L6smijJjnRiQ&s", 4.00);
+    
+insert into produto values(null, 3, "Beterraba Roxa", "Beterraba Roxa",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScFhlWXLPxwZFBcwdQlZRmpt-RdEBZrJTR3MSQNAIVc6Ukn1yJ&s", 4.00);
+    
+	-- INSERT PEDIDO    
+insert into pedido values(null, 1, 1, 2, '2020-01-28');
+    
+insert into pedido values(null, 2, 2, 2, '2020-01-28');
+    
+insert into pedido values(null, 3, 3, 2, '2020-01-28');
+    
+insert into pedido values(null, 4, 4, 2, '2020-01-28');
+    
+insert into pedido values(null, 5, 5, 2, '2020-01-28');
+    
+insert into pedido values(null, 6, 6, 2, '2020-01-28');
+    
+insert into pedido values(null, 7, 7, 2, '2020-01-28');
+    
+insert into pedido values(null, 8, 8, 2,'2020-01-28');
+    
+insert into pedido values(null, 9, 9, 2,'2020-01-28');
+    
+insert into pedido values(null, 10, 10, 2, '2020-01-28');
+
+insert into pedido values(null, 12, 12, 2, '2020-01-28');
+    
+insert into pedido values(null, 11, 11, 2, '2020-01-28');
+    
+insert into pedido values(null, 13, 13, 2, '2020-01-28');
+    
+insert into pedido values(null, 14, 14, 2, '2020-01-28');
+    
+insert into pedido values(null, 15, 15, 2, '2020-01-28');
+    
+	-- INSERT ENTREGA    
+insert into entrega values(null, 1, false);
+    
+insert into entrega values(null, 2, false);
+    
+insert into entrega values(null, 3, false);
+		
+insert into entrega values(null, 4, false);
+    
+insert into entrega values(null, 5, false);
+    
+insert into entrega values(null, 6, false);
+    
+insert into entrega values(null, 7, false);
+    
+insert into entrega values(null, 8,false);
+    
+insert into entrega values(null, 9, false);
+    
+insert into entrega values(null, 10, false);
+    
+insert into entrega values(null, 11, false);
+    
+insert into entrega values(null, 12, false);
+    
+insert into entrega values(null, 13, false);
+    
+insert into entrega values(null, 14, false);
+    
+insert into entrega values(null, 15, false);
