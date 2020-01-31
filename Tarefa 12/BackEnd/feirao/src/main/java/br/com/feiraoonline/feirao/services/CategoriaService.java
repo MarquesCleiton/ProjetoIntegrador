92117ace01254dc73039b63d45628e45bce1ab7f
Maywhy <mayara.cdb@gmail.com>
2020-01-31 10:50:02 -0300

    Criação do Service da Categoria

---
+++ Tarefa 12/BackEnd/feirao/src/main/java/br/com/feiraoonline/feirao/services/CategoriaService.java
@@ -0,0 +1,36 @@
+package br.com.feiraoonline.feirao.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.com.feiraoonline.feirao.dao.CategoriaRepo;
+import br.com.feiraoonline.feirao.model.Categoria;
+
+@Component
+public class CategoriaService implements ICategoriaService {
+	
+	@Autowired
+	private CategoriaRepo repo;
+
+	@Override
+	public List<Categoria> recuperaTodos() {				// aqui recupera todas as categorias
+		return (List<Categoria>) repo.findAll();
+	}
+
+	@Override
+	public Categoria recuperarPorId(int id) {				// aqui recupera as categorias pelo ID da mesma
+		return repo.findById(id).get();
+	}
+
+	@Override
+	public void novaCategoria(Categoria categoria) {		// aqui adiciona uma nova (caso a gnt adicione) categoria
+		repo.save(categoria);
+	}
+
+	@Override
+	public void atualizaCategoria(Categoria categoria) {	// aqui atualiza uma categoria (caso a gnt altere) que foi adicionada
+		repo.save(categoria);
+	}
+}
