7795417ad2d439b927d28e303d3c0c3266a8b0d0
Maywhy <mayara.cdb@gmail.com>
2020-02-06 15:03:25 -0300

    Barra de Rolagem

--- Teste/Teste Login/FeiraoOnline/src/app/duvidas/duvidas.component.ts
+++ Teste/Teste Login/FeiraoOnline/src/app/duvidas/duvidas.component.ts
@@ -10,6 +10,7 @@ export class DuvidasComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    window.scroll(0,0);
     $(document).ready(function(){
       // Add minus icon for collapse element which is open by default
       $(".collapse.show").each(function(){
